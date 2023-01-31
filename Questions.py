672e687d542bf3824cc7de91de708f44b2f257ac
Isaac Klausmeyer <118828742+iklau2023@users.noreply.github.com>
2023-01-31 17:35:39 -0600

    Create Questions.py

---
+++ Questions.py
@@ -0,0 +1,250 @@
+q= input('This question was written in a plane above Newfoundand and Labrador one of the provinces of country').lower()
+if q == 'canada':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which state is currently home to a series of glaciers found in a namesake national park').lower()
+if q == 'montana':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which 2 countries are divided by a Demilitarized zone though the border regions on either side of that are heavily fortified').lower()
+if q == 'north korea and south korea':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Wrigley Field, Lakeshore Drive, and the Field Museum are all landmarks found in which city').lower()
+if q == 'chicago':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which continent has the smallest number of native insect species').lower()
+if q == 'antarctica':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which country contains many of the tallest peaks in the Himalayas').lower()
+if q == 'nepal':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('You can travel from New Jersey to California entirely on which Interstate Highway').lower()
+if q == 'i-80':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which country might you visit on a family vacation to South America').lower()
+if q == 'peru':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which of these states is in the Midwest region of the USA').lower()
+if q == 'iowa':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('The Autobahn highway system connects cities such as Frankfurt and Cologne in which country').lower()
+if q == 'germany':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which African country contains a coastline on both the Mediterranean and Red sea').lower()
+if q == 'egypt':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('An EF5 Tornado the strongest on the Enhanced Fujita scale is most likely to occur in the metropolitan area of which US city').lower()
+if q == 'oklahoma city':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which of these countrys inhabitants predominantly speak a Romance language').lower()
+if q == 'spain':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Samoa the Marshall Island, Fiji, and Tonga are all island nations located in which ocean').lower()
+if q == 'pacific':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which Caribbean island nation has been led by a communist gov. for over 50 years').lower()
+if q == 'cuba':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which countrys flag is also known as the Union Jack and is also found on the flag of Australia').lower()
+if q == 'united kingdom' or 'uk':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which city is found in Mali and is used as a byword for a remote location').lower()
+if q == 'timbuktu':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which of these countries is considered to be part of Scandinavia').lower()
+if q == 'sweden':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('Which of these Us cities could a boat that only travels ').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
+q= input('').lower()
+if q == '':
+  print('Correct')
+else:
+  print('Wrong')
